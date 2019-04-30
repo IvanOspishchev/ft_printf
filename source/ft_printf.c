77ecfeb92a72ca514d70f8f4b6960a8399daba7e
Nathaniel Parker <nparker@at-r5.21-school.ru>
2019-04-30 17:36:27 +0300

    final_version

--- source/ft_printf.c
+++ source/ft_printf.c
@@ -3,101 +3,101 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nparker <nparker@student.42.fr>            +#+  +:+       +#+        */
+/*   By: vice-wra <vice-wra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/20 16:35:29 by nparker           #+#    #+#             */
-/*   Updated: 2019/04/20 16:36:59 by nparker          ###   ########.fr       */
+/*   Updated: 2019/04/29 18:30:25 by vice-wra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		find_the_flags(char **str)
-{
-	int i;
-
-	i = 0;
-	while (((*str)[i] && !ft_isalpha((*str)[i])) || ((*str)[i] == 'l' || (*str)[i] == 'L' || (*str)[i] == 'h'))
-	{
-		if ((*str)[i + 1] && (*str)[i + 1] == '%')
-		{
-			i++;
-			break ;
-		}
-		i++;
-	}
-	return (i);
-}
-
 void		get_queue(char *format, t_queue *queue)
 {
-	char *str;
-	int i;
-	char *temp;
+	char	*str;
+	int		i;
 
 	while (format && ft_strchr(format, '%'))
 	{
-		i = 0;
 		str = ft_strsub(format, 0, ft_strchr(format, '%') - format);
 		if (*str)
 			queue_push(queue, str);
 		ft_strdel(&str);
-		temp = format;
 		format = ft_strchr(format, '%');
 		i = find_the_flags(&format);
 		str = ft_strsub(format, 0, i + 1);
 		queue_push(queue, str);
 		ft_strdel(&str);
-		format = format + i + 1;
+		if (format[i])
+			format = format + i + 1;
+		else
+			format = format + i;
 	}
-	i = 0;
-	while (format[i])
-		i++;
+	i = ft_strchr(format, '\0') - format;
 	str = ft_strsub(format, 0, i);
 	queue_push(queue, str);
 	ft_strdel(&str);
 }
 
-void 	decide(t_queue *q, t_string *cust_str, va_list *args)
+void		put_into_cust_str(t_fs *form_string, t_string *cust_str, char *str)
 {
-	char *str;
-	t_fs form_string;
-	int i;
-	char *temp;
+	if (str)
+	{
+		if (form_string->type == 'c' &&
+				g_crutch == 1)
+		{
+			if (!ft_strchr(form_string->flags, '-'))
+			{
+				str_pushstr(cust_str, str);
+				str_pushchar(cust_str, '\0');
+			}
+			else
+			{
+				str_pushchar(cust_str, '\0');
+				str_pushstr(cust_str, str);
+			}
+		}
+		else
+			str_pushstr(cust_str, str);
+	}
+}
 
+void		decide(t_queue *q, t_string *cust_str, va_list *args)
+{
+	char	*str;
+	t_fs	form_string;
+
+	fs_init(&form_string);
 	while (q->size)
 	{
-		i = 0;
 		if (!ft_strchr(str = queue_pop(q), '%'))
-			while (str[i])
-				str_pushchar(cust_str, str[i++]);
+			str_pushstr(cust_str, str);
 		else
 		{
-			temp = str;
-			str = ft_strdup(temp + 1);
-			ft_strdel(&temp);
-			get_fs(&str, &form_string);
-			prepare_item(&form_string);
-			handler_item(args, &form_string, &str);
-			while (str[i])
-				str_pushchar(cust_str, str[i++]);
+			move_char_pointer(&str);
+			process_fs(&str, &form_string, args);
+			put_into_cust_str(&form_string, cust_str, str);
+			fs_destroy(&form_string);
 		}
 		ft_strdel(&str);
 	}
-	ft_strdel(&(form_string.flags));
 }
 
-int					ft_printf(char *format, ...)
+int			ft_printf(char *format, ...)
 {
 	va_list			args;
 	t_string		str;
 	t_queue			queue;
 
+	if (!format || *format == '\0' || (ft_strlen(format) == 1 &&
+												*format == '%'))
+		return (0);
 	va_start(args, format);
 	queue_create(&queue);
 	get_queue(format, &queue);
 	str = str_create_size(ft_strlen(queue.tail->data));
 	decide(&queue, &str, &args);
 	write(1, str.data, str.size);
+	ft_strdel(&str.data);
 	return ((int)str.size);
 }
